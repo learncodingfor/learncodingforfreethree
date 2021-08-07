c03933113933d514ca28bdda42f2e2e603260d09
apple <78810546+pytube832@users.noreply.github.com>
2021-08-07 15:36:25 -0400

    Update cookiesenabled.js

--- home/cookiesenabled.js
+++ home/cookiesenabled.js
@@ -1,6 +1,6 @@
 const cookie = {
   isenabled: true,
-  cookieenabled: function() {
+  cookiesenabled: function() {
     //checks if cookies are enabled if not it displays a message telling the user to enable cookies
     if(navigator.cookieEnabled == true){console.log("cookies Enabled!")}else{alert("Enable cookies")}
   }
@@ -9,4 +9,4 @@ const cookie = {
 const cookiesenabled = Object.create(cookie);
 // "name" is a property set on "me", but not on "person"
 // inherited properties can be overwritten
-cookiesenabled.cookieenabled();
+cookiesenabled.cookiesenabled();

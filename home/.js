5a6da0843bcb99f926f76532c1ba1c65c1350bf2
apple <78810546+pytube832@users.noreply.github.com>
2021-08-07 14:41:58 -0400

    Update and rename creatediv.js to .js

---
+++ home/.js
@@ -0,0 +1,6 @@
+const isCookieEnabled = function() {
+  div:false;
+  isEnabled:function(){
+    if(navigator.cookiesEnabled){alert("cookies are enabled")}
+  }
+}
--- home/creatediv.js
+++
@@ -1,8 +0,0 @@
-const createDiv = function() {
-  div:false;
-  displaydiv:function(){
-    var d = document.createElement("div");
-    d.innerHTML = "this is a div element";
-    document.body.appendChild(d);
-  }
-}

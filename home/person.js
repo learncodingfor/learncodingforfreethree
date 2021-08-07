3eba8e4ae6e8fbdde19987a708eff199e4d49289
apple <78810546+pytube832@users.noreply.github.com>
2021-08-07 14:24:24 -0400

    Update person.js

--- home/person.js
+++ home/person.js
@@ -1,7 +1,7 @@
 const person = {
   isHuman: false,
   printIntroduction: function() {
-    console.log(`My name is ${this.name}".Am I human?" ${this.isHuman}`);
+    alert(`My name is ${this.name}".Am I human?" ${this.isHuman}`);
   }
 };
 

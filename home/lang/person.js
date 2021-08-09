6036bcdedf1ea6545515e776dc9f2a8765adab57
apple <78810546+pytube832@users.noreply.github.com>
2021-08-09 12:33:22 -0400

    Update person.js

--- home/lang/person.js
+++ home/lang/person.js
@@ -4,3 +4,8 @@ const Person = {
     alert("My name is ${this.name}. I am ${this.age} years old. Am i human? ${this.human}");
   }
 }
+const person = Object.create(Person);
+person.age = 23;
+person.name = "bob";
+person.isHuman = true;
+person.printperson();

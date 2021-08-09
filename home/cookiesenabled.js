2d4f4f3150d8a293ccc25fcbc356ade1963310d0
apple <78810546+pytube832@users.noreply.github.com>
2021-08-09 11:40:40 -0400

    Update cookiesenabled.js

--- home/cookiesenabled.js
+++ home/cookiesenabled.js
@@ -5,7 +5,7 @@ const CookiesEnabled = {
     if(navigator.cookieEnabled = true){alert("cookies Enabled");}else{alert("Enable cookies");}
    }
   isenableddocumentwrite:function(){
-    if(navigator.cookieEnabled = true){console.log("Cookies Enabled!")}else{document.write("Enable Cookies!})
+    if(navigator.cookieEnabled = true){console.log("Cookies Enabled!")}else{document.write("Enable Cookies")}
   }
  };
 const cookies = Object.create(CookiesEnabled);

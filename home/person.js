d6fa84d835bdbad0094ef6b2382327bcc3f08a3f
apple <78810546+pytube832@users.noreply.github.com>
2021-08-09 11:34:45 -0400

    Delete person.js

--- home/person.js
+++
@@ -1,9 +0,0 @@
-const person = {
-  isHuman: false,
-  printIntroduction: function() {
-    alert(`My name is ${this.name}".Am I human?" ${this.isHuman}`);
-  }
-};
-
-const me = Object.create(person);
-// expected output: "My name is Matthew. Am I human? true"

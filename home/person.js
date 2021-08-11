f0e5abfc9a06f893f4d178a7cf10c74f87ad5b8f
apple <78810546+pytube832@users.noreply.github.com>
2021-08-11 12:40:26 -0400

    Update person.js

--- home/person.js
+++ home/person.js
@@ -1,7 +1,7 @@
 const Person = {
   isHuman:false,
   printIntroduction:function() {
-    alert('hello my name is ${this.firstName} ${this.lastName}. Am i human? ${this.isHuman} Am i happy? ${this.mood} my gender is $(this.gender)');
+    alert('hello my name is ${this.firstName} ${this.lastName}. Am i human? ${this.isHuman} Am i happy? ${this.mood} my gender is ${this.gender}');
   }
 };
 const person = Object.create(Person);

6f354e471b230d2cf07d20507440be2823b86a84
apple <78810546+pytube832@users.noreply.github.com>
2021-08-11 12:24:11 -0400

    Update cookiesenabled.js

--- home/cookiesenabled.js
+++ home/cookiesenabled.js
@@ -1,9 +1,1 @@
-const CookiesEnabled = {
-  Enabled: true,
-  isenabled: function() {
-    //checks if cookies are enabled if not it displays a message telling the user to enable cookies
-    if(navigator.cookieEnabled){console.log("cookies enabled!")}else{alert("Enable Cookies!")}
-  }
-};
-const cookies = Object.create(CookiesEnabled);
-cookies.isenabled();
+

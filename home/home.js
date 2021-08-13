91ad1c166c920cd14e89e37330de6f42d03de528
apple <78810546+pytube832@users.noreply.github.com>
2021-08-13 14:29:15 -0400

    Update home.js

--- home/home.js
+++ home/home.js
@@ -1,9 +1,9 @@
 let script = document.createElement("script");
-script.src = "https://cdnjs.cloudflare.com/ajax/libs/loadash.js/4.3.0/loadash.js"
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/loadash.js/4.3.0/loadash.js";
 document.head.append(script);
 script.onload = function(){
-  alert(_.VERSION)
+  alert(_.VERSION);
 }
 script.onerror = function(){
-  this.src = "error.js"
+  this.src = "error.js";
 }

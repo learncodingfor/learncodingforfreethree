6d4add3ec37907f728bd136a4c40f3e94f0d334f
apple <78810546+pytube832@users.noreply.github.com>
2021-08-22 19:10:39 -0400

    Update home.py

--- home/home.py
+++ home/home.py
@@ -2,3 +2,23 @@ import flask
 @app.route("/your/webservice")
 def my_webservice():
     return jsonify(result=some_function(**request.args)) 
+import cgi
+import cgitb; cgitb.enable()
+import sqlite3
+
+mydb = 'messenger.db'
+conn = sqlite3.connect(mydb)
+cursor = conn.cursor()
+
+print('alex')
+
+"""
+form = cgi.FieldStorage()
+Sender = form.getvalue('UserName')
+Reciever = form.getvalue('Class')
+message = form.getvalue('Message')
+val = {"Sender":Sender, "Reciever":Reciever, "message":message}
+cursor.execute('''INSERT INTO Message (Message, SenderID, ClassID) VALUES (:message, :Sender, :Reciever)''',val)
+conn.commit()
+cursor.close()
+"""
